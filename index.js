68966da091f12ed9fa8b79c4637522cab2eb6786
maisha850 <tasnimmaisha08@gmail.com>
2025-12-10 02:16:40 +0600

    added payments route

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ async function run() {
     const db = client.db('ticket_Bari')
     const ticketCollection = db.collection('tickets')
     const bookCollection = db.collection('book')
+    const paymentCollection = db.collection('payment')
     const userCollection = db.collection('users')
     app.get('/tickets', async(req ,res)=>{
       const result = await ticketCollection.find().toArray()
@@ -99,38 +100,7 @@ async function run() {
       const result = await ticketCollection.deleteOne(query)
       res.send(result)
     })
-    // payment
-//      app.post('/payment-checkout-session', async(req ,res)=>{
-//       const paymentInfo = req.body
-//       const amount = parseInt(paymentInfo.total) * 100
-//       const quantity = parseInt(paymentInfo.quantity)
-//       const session = await stripe.checkout.sessions.create({
-  
-//   line_items: [
-//     {
-//    price_data: {
-//     currency: 'usd',
-//     unit_amount: amount,
-//     product_data : {
-//       name : `please pay for ${paymentInfo.title}`
-//     }
-//    },
-//       quantity: quantity,
-//     },
-//   ],
-//   mode: 'payment',
-//   metadata: {
-//     ticketId : paymentInfo.ticketId,
-//    title : paymentInfo.title,
- 
-//   },
-//   customer_email : paymentInfo.userEmail,
-//   success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-// cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
-// });
-// res.send({url : session.url})
-
-//     })
+   
 
 app.post('/payment-checkout-session', async (req, res) => {
   try {
@@ -152,13 +122,14 @@ app.post('/payment-checkout-session', async (req, res) => {
               name: `please pay for ${paymentInfo.title}`
             }
           },
-          quantity,
+          quantity
         },
       ],
       mode: 'payment',
       metadata: {
-        ticketId: paymentInfo.ticketId,
+        ticketsId: paymentInfo.ticketsId,
         title: paymentInfo.title,
+        quantity: Number(paymentInfo.quantity)
       },
       customer_email: paymentInfo.userEmail,
       success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
@@ -171,6 +142,82 @@ app.post('/payment-checkout-session', async (req, res) => {
     res.status(400).send({ error: err.message });
   }
 });
+// payment succuss 
+  app.patch('/payment-success', async (req, res) => {
+    try {
+        const sessionId = req.query.session_id;
+
+        if (!sessionId) return res.status(400).send({ success: false, message: "Session ID missing" });
+
+        const session = await stripe.checkout.sessions.retrieve(sessionId);
+        console.log('Stripe session:', session);
+  const transactionId = session.payment_intent
+  const query = {transactionId : transactionId}
+  const paymentExists = await paymentCollection.findOne(query)
+  if(paymentExists){
+    return res.send({message: 'already exists' , transactionId })
+  }
+          
+        if (session.payment_status === 'paid') {
+          const ticketsId = session.metadata.ticketsId;
+
+            // const query = { _id: new ObjectId(ticketId) };
+            const query = {_id : new ObjectId(ticketsId)}
+            const update = { $set: 
+              { 
+                status: "paid"
+               }
+               };
+              
+            const result = await bookCollection.updateOne(query, update);
+
+            const payment = {
+                amount: session.amount_total / 100,
+                currency: session.currency,
+                customerEmail: session.customer_email,
+          ticketsId:ticketsId,
+                title: session.metadata.title,
+                transactionId: session.payment_intent,
+                paymentStatus: session.payment_status,
+             quantity:session.metadata.quantity,
+                paidAt: new Date(),
+            };
+
+            const resultPayment = await paymentCollection.insertOne(payment);
+        
+
+            // ✅ Send only one response here
+        return res.send({
+                success: true,
+                modifyTicket: result,
+                paymentInfo: resultPayment,
+                transactionId: session.payment_intent,
+                amount: session.amount_total / 100
+            });
+        }
+
+        // Payment not completed
+        return res.send({ success: false, message: "Payment not completed yet" });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, message: "Server error" });
+    }
+});
+app.get('/payments',  async(req , res)=>{
+  const email = req.query.email
+  const query = {}
+  if(email){
+    query.customerEmail = email
+  }
+ 
+ const  result = await paymentCollection.find(query).sort({paidAt: -1}).toArray()
+ res.send(result)
+
+})
+
+
+
 
     // book
     app.get('/myBookedTickets/:email' , async(req , res)=>{
@@ -193,6 +240,9 @@ app.post('/payment-checkout-session', async (req, res) => {
        const result = await bookCollection.insertOne(books)
       res.send(result)
     })
+    // app.get('/requested-bookings' , async(req , res)=>{
+    //   const 
+    // })
 
     // role
     app.get('/users/role/:email' , async(req , res)=>{
